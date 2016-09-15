a110e1704276c979327580f61a18c881404f59c9
kohrVid <kohrVid@zoho.com>
2016-09-15 20:32:50 +0100

    Added inserts and queries to the edXMEAN app

--- edxMEAN/index.js
+++ edxMEAN/index.js
@@ -7,13 +7,28 @@ mongodb.MongoClient.connect(uri, function (error, db) {
     process.exit(1);
   }
 
-  db.collection("sample").insert({x: 1}, function (error, result){
+  var doc = {
+    title: "Jaws",
+    year: 1975,
+    director: "Steven Spielberg",
+    rating: "PG",
+    ratings: {
+      critics: 80,
+      audience: 97
+    },
+    screenplay: ["Peter Benchley", "Carl Gotlieb"]
+  };
+
+  db.collection("movies").insert(doc, function (error, result){
     if (error) {
       console.log(error);
       process.exit(1);
     }
+    
+    var query = { year: 1975, rating: "PG" };
+    var query2 = { "ratings.audience": { "$gte": 90 } };
 
-    db.collection("sample").find().toArray(function (error, docs) {
+    db.collection("movies").find(query2).toArray(function (error, docs) {
       if (error) {
 	console.log(error);
 	process.exit(1);
@@ -25,5 +40,6 @@ mongodb.MongoClient.connect(uri, function (error, db) {
       });
       process.exit(0);
     });
+
   });
 });
