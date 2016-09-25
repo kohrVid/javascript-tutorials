a24ca832dde4dc016cee89f4764b12acc0591d4b
kohrVid <kohrVid@zoho.com>
2016-09-25 17:41:00 +0100

    Added Page Testing and Cross-Page Testing to the Meadowlark app

--- oreilly/NodeAndExpress/MeadowlarkTravel/meadowlark.js
+++ oreilly/NodeAndExpress/MeadowlarkTravel/meadowlark.js
@@ -3,11 +3,25 @@ var express = require("express");
 var app = express();
 
 var handlebars = require("express3-handlebars")
-    .create({ defaultLayout: "main" });
+    .create({ defaultLayout: "main",
+   helpers: {
+     section: function (name, options) {
+       if(!this._sections) this_sections = {};
+       this._sections[name] = options.fn(this);
+       return null;
+     }
+   } 
+});
 var fortune = require("./lib/fortune.js");
 
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
+
+app.use(function (req, res, next) {
+  res.locals.showTests = app.get("env") !== "production" &&
+    req.query.test === "1";
+  next();
+});
 
 app.set("port", process.env.PORT || 8081);
 
@@ -18,7 +32,18 @@ app.get("/", function (req, res) {
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", { fortune: fortune.getFortune() });
+  res.render("about", { 
+    fortune: fortune.getFortune(),
+    pageTestScript: "/qa/tests-about.js"
+  });
+});
+
+app.get("/tours/hood-river", function (req, res) {
+  res.render("tours/hood-river");
+});
+
+app.get("/tours/request-group-rate", function (req, res) {
+  res.render("tours/request-group-rate");
 });
 
 app.use(function (req, res) {
