5a3d04536282052e49659fae19e65d0662e91dc9
kohrVid <kohrVid@zoho.com>
2016-09-26 03:27:51 +0100

    Added Unit tests, JSHint, Linkchecker and Grunt to Meadowlark app

--- oreilly/NodeAndExpress/MeadowlarkTravel/meadowlark.js
+++ oreilly/NodeAndExpress/MeadowlarkTravel/meadowlark.js
@@ -3,17 +3,17 @@ var express = require("express");
 var app = express();
 
 var handlebars = require("express3-handlebars")
-    .create({ defaultLayout: "main",
-   helpers: {
-     section: function (name, options) {
-       if(!this._sections) this_sections = {};
-       this._sections[name] = options.fn(this);
-       return null;
-     }
+  .create({ 
+     defaultLayout: "main",
+     helpers: {
+       section: function (name, options) {
+	 if(!this._sections) this_sections = {};
+	 this._sections[name] = options.fn(this);
+	 return null;
+       }
    } 
 });
 var fortune = require("./lib/fortune.js");
-
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
